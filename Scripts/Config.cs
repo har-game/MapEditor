3dce0af80f373a29a6e49c589e1b5dc210f519c6
wmysterio <wmysterio@ukr.net>
2023-09-04 19:59:26 +0300

    Add files via upload

--- Scripts/Config.cs
+++ Scripts/Config.cs
@@ -4,11 +4,10 @@ namespace HAR {
 	
 	public sealed class Config {
 		
-		public static bool IsValidModName( string name ) => Regex.IsMatch( name, @"^[a-z0-9_]{1,32}$", RegexOptions.Compiled );
-		public static bool IsValidMapName( string name ) => Regex.IsMatch( name, @"^[123456789]\d{0,}$", RegexOptions.Compiled );
+		public static bool IsValidModName( string name ) => Regex.IsMatch( name, @"^[a-z][a-z0-9_]{0,31}$", RegexOptions.Compiled );
+		public static bool IsValidMapName( string name ) => Regex.IsMatch( name, @"^[123456789]\d{0,5}$", RegexOptions.Compiled );
 		
 		public static string ModsDirectory;
-		public static float SectionSize = 100f;
 
 	}
 	
